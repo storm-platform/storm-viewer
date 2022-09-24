7d7ecc8e996e873f0ee31c4b819f99fd130e0580
Felipe Carlos <efelipecarlos@gmail.com>
2022-09-24 08:04:45 -0300

    viewer: adding header

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,20 @@ import ReactDOM from "react-dom/client";
 
 import "semantic-ui-css/semantic.min.css";
 
+import { Container } from "semantic-ui-react";
+
+import "./main.css";
 import { WorkflowView } from "./WorkflowView.jsx";
+import { WorkflowHeader } from "./WorkflowHeader.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <WorkflowView apiUrl={import.meta.env.VITE_API_ADDRESS} />
+  <Container>
+    <WorkflowHeader />
+    <div className={"viewer-app-container"}>
+      <WorkflowView
+        apiUrl={import.meta.env.VITE_API_ADDRESS}
+        apiKey={import.meta.env.VITE_API_KEY}
+      />
+    </div>
+  </Container>
 );
